cede094f4618c9cf2accc7dac8ec68f4be2f54bb
Karen-Anahi-Padron-Martinez <karenpadron0608@gmail.com>
2026-06-09 16:55:45 -0600

    feat: add AlertaScreen with AlertDialog MD3, spinner and preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21"
-
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -15,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "mx.utng.kapm.smarthealthmonitor"
-        minSdk = 26
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -52,6 +51,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.jetbrains.kotlin.reflect)
     implementation("androidx.navigation:navigation-compose:2.8.0")
     implementation(libs.play.services.wearable)
     implementation(libs.kotlinx.coroutines.play.services)
@@ -61,14 +61,10 @@ dependencies {
     implementation(libs.kotlinx.coroutines.guava)
     // Room
     // Room
-    val roomVersion = "2.6.1"
+    val roomVersion = "2.8.4"
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
-
-
-
-
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
